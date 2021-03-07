27afc5adca59d63c795924d08c868a2352c89e19
Samuel Casey <samuelacasey@gmail.com>
2021-03-07 17:24:43 +0000

    fixed formatting v2

--- config.js
+++ config.js
@@ -1,9 +1,11 @@
+/** @format */
+
 var Config = {
-    HUMAN_PLAYER: 1,
+	HUMAN_PLAYER: 1,
 	COMPUTER_AI: 2,
-    
+
 	ROWS_SIZE: 6,
 	COLUMNS_SIZE: 7,
 
-	WINNING_SCORE: 100000
-}
+	WINNING_SCORE: 100000,
+};
